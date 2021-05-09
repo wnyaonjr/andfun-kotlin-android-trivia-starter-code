0abb97df756b20b9ec91b8b33af13bb76257ac31
wnyaonjr <wnyaonjr@gmail.com>
2021-05-09 20:52:34 +0800

    - add navigation resource - add NavHostFragment in activity_main layout - add title fragment in the navigation.xml - add game fragment in the navigation.xml - connect title fragment to game fragment - create onclick listener in title fragment to navigate to game fragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -13,8 +14,13 @@ class TitleFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding = FragmentTitleBinding.inflate(inflater, container, false)
+
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+
         return binding.root
     }
 }
