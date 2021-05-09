c2d37ef13892d32d56a43ec1cc5d73f2e148f393
wnyaonjr <wnyaonjr@gmail.com>
2021-05-09 21:45:16 +0800

    - pop to game fragment inclusive for game over and won - add action connecting gameWonFragment to the gameFragment - add action connecting gameOverFragment to the gameFragment

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -22,15 +22,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
 class GameWonFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_won, container, false)
+            inflater, R.layout.fragment_game_won, container, false
+        )
+
+        binding.nextMatchButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_gameFragment)
+        )
+
         return binding.root
     }
 }
